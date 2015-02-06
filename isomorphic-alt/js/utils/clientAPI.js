0e376086c1d11dc7ba603dcec3053d8a30a698ff
Hanusz Leszek <leszek.hanusz@gmail.com>
2015-02-06 16:04:09 +0100

    Remove dummy ping socket.io function

--- isomorphic-alt/js/utils/clientAPI.js
+++ isomorphic-alt/js/utils/clientAPI.js
@@ -7,15 +7,6 @@ var Promise = require('es6-promise').Promise;
 var socket = io('localhost:' + config.port);
 
 module.exports = {
-    ping: function () {
-        return new Promise(function (resolve) {
-            console.log('emit ping');
-            socket.emit('ping', 'blah', function (data) {
-                console.log(' received ' + data);
-                resolve();
-            });
-        });
-    },
     cartCheckout: function (products) {
         return new Promise(function (resolve, reject) {
             socket.emit('cartCheckout', products, function (data) {
--- isomorphic-alt/serverAPI.js
+++ isomorphic-alt/serverAPI.js
@@ -8,11 +8,6 @@ function serverAPI (http) {
     io.on('connection', function (socket) {
         console.log('a user connected');
 
-        socket.on('ping', function (data, fn) {
-            console.log(' ping received');
-            fn('pong');
-        });
-
         socket.on('cartCheckout', function (productsWanted, fn) {
             console.log('YOU WANT:\n');
             console.log(productsWanted);
